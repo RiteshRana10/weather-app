32924b4fe09e69141860a56df6b8be0ebfb7528f
Ritesh Singh <riteshranarana.live@gmail.com>
2019-12-20 17:51:28 +0800

    Create folder structure, global css files and HomePage component
    Removed default app files of boilerplate code and navigating to new home page component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../HomePage/HomePage';
+
+
+// This is Dashboard for Weather-Forecasting 
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div className="main_comp" >
+       <p> Home Page </p>
+        </div>
+        );
+    }
+}
+
