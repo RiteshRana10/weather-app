57f2f73054bcb45f0105550be2554cfec0e0c01d
Ritesh Singh <riteshranarana.live@gmail.com>
2019-12-20 17:21:46 +0800

    Setting Boilerplate Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p>weather-app</p>       
+    </div>
+  );
+}
+
+export default App;
