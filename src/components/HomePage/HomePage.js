e83baf575e5dbff3decc5208663efe177f6f9068
Ritesh Singh <riteshranarana.live@gmail.com>
2019-12-23 14:40:39 +0800

    Create new UI controls for home page and add header component in Home Page
    UI controls for showing temperature, wind, precipitation, humidity, Location, Date and Time, styling for home page using scss

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,19 +1,135 @@
 import React, { Component } from 'react';
-import "./HomePage.scss"
 import Header from '../Header/Header';
+import "./HomePage.scss";
+import Tide from '../Tide/Tide';
+import humidityIcon from '../../assets/images/humidity.png';
+import cloud3 from '../../assets/images/cloud3.png';
+import precipitation from '../../assets/images/precipitation.png'
+import wind from '../../assets/images/wind.png';
+import cloudIcon from '../../assets/images/cloud.png';
+import temp from '../../assets/images/temp.png';
 
+const weatherData = {
+    "cod": "200",
+    "city": {
+        "id": 2643743,
+        "name": "Singapore",
+        "coord": {
+            "lon": -0.1277,
+            "lat": 51.5073
+        },
+        "country": "GB"
+    },
+    "message": 0.353472054,
+    "list": [
+        {
+            "dt": 1560384000,
+            "humidity": 72.96,
+            "pressure": 744,
+            "temp": {
+                "average": 289.42,
+                "average_max": 292.55,
+                "average_min": 285.12,
+                "record_max": 298.95,
+                "record_min": 281.34
+            },
+            "wind_speed": 2.74
+        }]
+};
 
-// This is Dashboard for Weather-Forecasting 
+const TodayDate = new Date();
+const month = TodayDate.getMonth();
+const date = TodayDate.getDate();
+
+// This is main Dashboard Page for Weather-Forecasting 
 export default class HomePage extends Component {
+    constructor() {
+        super();
+    }
+    // setting data into the state
+    state = {
+        temp: "29.4",
+        humidity: weatherData.list[0].humidity,
+        weather: "Cloudy",
+        cityName: weatherData.city.name,
+        wind_speed: weatherData.list[0].wind_speed,
+        precipitation: "87",
+        pressure: weatherData.list[0].pressure
+
+    }
+    // Getting full name of the day
+    convertDays() {
+        const day = TodayDate.getDay();
+        let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+        let currentDay = daylist[day];
+        return currentDay;
+    }
+    formatAMPM() {
+        let hours = TodayDate.getHours();
+        let minutes = TodayDate.getMinutes();
+        const ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+    // Getting Full name of the month
+    convertMonth() {
+        let monthlist = ["January", "February", "March", "April ", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let currentMonth = monthlist[month];
+        return currentMonth + " " + date;
+    }
     render() {
         return (
-            <div className="main_comp" >
-                <div>
-                    <Header></Header>
+            <div className="main-comp" >
+                <Header></Header>
+                <div className="home-page">
+                    <div className="city-data">
+                        <span className="city-info">{this.state.cityName}</span>
+                        <span className="date-time">{this.convertMonth()}, {this.convertDays()} {this.formatAMPM()}</span>
+                        <span className="date-time">{this.state.weather}</span>
+                        <img className="cloud-icon" src={cloudIcon} alt="Cloudy" />
+                        <span className="temperature-text">{this.state.temp} &#8451;</span>
+                    </div>
+
+                    <div className="weather-info">
+                        <table className="weather-table">
+                            <thead>
+                                <tr>
+                                    <th className="weather-table-heading">Weather Report</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="weather-data">
+                                        <img className="text-icon" src={temp} alt="Temperature" />
+                                        <span className="weather-text">{this.state.pressure} mmHg</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="weather-data">
+                                        <img className="text-icon" src={humidityIcon} alt="Humidity" />
+                                        <span className="weather-text">{this.state.humidity} %</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="weather-data">
+                                        <img className="text-icon" src={wind} alt="Wind" />
+                                        <span className="weather-text">{this.state.wind_speed} km/h</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="weather-data">
+                                        <img className="text-icon" src={precipitation} alt="Precipitation" />
+                                        <span className="weather-text">{this.state.precipitation} %</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div>
-                    <p> Home Page </p>
-                </div>
+                <Tide></Tide>
             </div>
         );
     }
