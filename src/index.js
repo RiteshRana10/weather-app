381c4439ceaa5dd761d5533e7c8d15bea7e76170
Ritesh Singh <riteshranarana.live@gmail.com>
2019-12-23 12:34:02 +0800

    Add icons and images in asset folder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage/HomePage';
 
 import * as serviceWorker from './serviceWorker';
 
+// Loading Home page in the root div for index.html
 ReactDOM.render(<HomePage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
