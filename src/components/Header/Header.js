db152078034231ff7f8f6d77c6caa099a89803ff
Ritesh Singh <riteshranarana.live@gmail.com>
2019-12-23 12:57:44 +0800

    Create header UI with logo and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import "./Header.scss"
+import "./Header.scss";
+import notification from '../../assets/images/notification.png'
+import logo from '../../assets/images/logo.png'
 
 // This is fixed top bar component of the application.
 export default class Header extends Component {
     render() {
         return (
-            <div className="header_bar">
-                <label className="header_text1">
-                    Weather Forecasting
-        </label>
+            <div className="header-bar">
+                <img className="logo-icon" src={logo} alt="Weather Forecast" />
+                <span className="header-text">Weather Forecast</span>
+                <img className="notification-icon" src={notification} alt="Notification" />
             </div>
         )
     }
